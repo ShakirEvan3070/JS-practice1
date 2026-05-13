5a0a9a8adfcbd5b0f60d0e5f848142fc670da042
ShakirEvan3070 <ausshakirahmed@gmail.com>
2026-05-13 10:12:23 +0600

    First commit

---
+++ JS/1.js
@@ -0,0 +1,100 @@
+// const country = 'Bangladesh';
+// const division = 'Dhaka';
+// const district = 'Gaz   ipur';
+
+// console.log(country)
+// console.log(division)
+// console.log(district)
+
+// console.log(district.length)
+
+// const subject = "Chemistry";
+// const book = "chemistry";
+// const book = "Chemistry";
+
+// if(subject === book){
+//     console.log("Both of them are same items")
+// }
+// else{
+//     console.log("Both of them are different items");
+// }
+
+// const school = "Bangladesh Bank Adarsha High School";
+// console.log(school.toLowerCase());
+// console.log(school.toUpperCase());
+
+// const first = "Aus Shakir Ahmed"
+// const last = "Evan"
+
+// const full = first + ' ' + last;
+// console.log(full);
+
+// const fullName = first.concat(' ',last)
+// console.log(fullName);
+
+// const present = first.includes("A");
+// console.log(present)
+
+// const sentence = "I am a student of CSE";
+// let = reverse = '';
+
+// for (const letter of sentence){
+    // console.log(letter)
+//     reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+// const person = {
+//     name: "Shakir Ahmed",
+//     age: 22,
+//     profession: "Student",
+//     country: "Bangladesh"
+// };
+// console.log(person)
+
+// function hello(){
+//     console.log(" I am calling a function")
+// }
+// hello();
+
+// function moneybag(){
+//        var money = 1000;
+//     console.log("I have " + money + " Dollars") 
+// }
+// moneybag()
+
+
+// function biyog(a,b){
+//     var Sub = a - b;
+//     // console.log(Sub);
+//     return Sub;
+// }
+// var result1 = biyog(10,5);
+// console.log(result1)
+
+// function gun(a,b){
+//     var mul = a * b;
+//     return mul;
+// }
+// var result2 = gun(10,5);
+// console.log(result2)
+
+// function vag(a,b){
+//     var div = a/b;
+//     return div;
+// }
+// var result3 = vag(10,5);
+// console.log(result3)
+
+
+
+
+
+
+
+
+
+
+
+
+
