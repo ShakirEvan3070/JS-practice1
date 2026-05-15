688fef576287d0247755872b7939bffa3cb17fbb
ShakirEvan3070 <ausshakirahmed@gmail.com>
2026-05-15 17:48:50 +0600

    Creating element

---
+++ JS/DOM/child.js
@@ -0,0 +1,13 @@
+// create element and set innertext
+// const newChild = document.createElement("li");
+// newChild.innerText = "New born footballer here";
+const newChild1 = document.createElement('li');
+newChild1.innerText = "Cherry";
+
+// find the parent element 
+// const playerList = document.getElementById("player-list");
+const fruitList = document.getElementById("fruit-list");
+
+// Append the child to the parent 
+// playerList.appendChild(newChild);
+fruitList.appendChild(newChild1)
