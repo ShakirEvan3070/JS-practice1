bb2f04f6ca6f1a095827748ee906c4b46c4079d4
ShakirEvan3070 <ausshakirahmed@gmail.com>
2026-05-17 11:03:14 +0600

    color changing

---
+++ JS/DOM/event.js
@@ -0,0 +1,26 @@
+// function makeYellow(){
+//     document.body.style.backgroundColor = 'yellow';
+//     document.body.style.color='red';
+// }
+// function makeRed(){
+//     document.body.style.backgroundColor = 'red';
+//     document.body.style.color='navy';
+// }
+// function makeGreen(){
+//     document.body.style.backgroundColor = 'green';
+//     document.body.style.color = "Aqua";
+// }
+// function makeAqua(){
+//     document.body.style.backgroundColor = 'aqua';
+//     document.body.style.color = 'maroon';
+// }
+
+// const buttons = document.querySelectorAll('.btn')
+
+// for (const button of buttons){
+//     button.addEventListener('click',function(){
+//         button.style.backgroundColor = 'green';
+//         button.style.color = 'white';
+
+//     })
+// }
