009a5772fcfae25ade087bc40a52df382408e10c
ShakirEvan3070 <ausshakirahmed@gmail.com>
2026-05-17 12:15:50 +0600

    name update using the DOM

--- JS/DOM/event.js
+++ JS/DOM/event.js
@@ -25,9 +25,39 @@
 //     })
 // }
 
-document.getElementById('btn-logged-in').addEventListener('click',function(){
-    console.log('button clicked')
+// document.getElementById('btn-logged-in').addEventListener('click',function(){
+//     console.log('button clicked')
 
-    const newTitle = document.getElementById('logged-in');
-    newTitle.innerText = 'User logged in successfully';
+//     const newTitle = document.getElementById('logged-in');
+//     newTitle.innerText = 'User logged in successfully';
+// })
+
+// Get connected with the button first
+// document.getElementById('btn-update').addEventListener('click',function(){
+//     console.log('Button clicked');
+
+// // Connect with the input field and the field item 
+//     const newName = document.getElementById('name-deatails').value;
+//     console.log(newName);
+
+//     // Print the new name in the name field
+//     const nameField = document.getElementById('name');
+//     nameField.innerText = newName;
+// })
+
+document.getElementById('NameUpdate').addEventListener('click',function(){
+    // console.log("Button has been clicked")
+
+const nameField = document.getElementById('input1').value;
+
+const NewName = document.getElementById('Name1')
+NewName.innerText = nameField;
+NewName.style.color = 'green';
+NewName.style.backgroundColor = 'yellow';
+NewName.style.width = '60px';
+NewName.style.margin = '10px ';
+NewName.style.padding = '15px';
+NewName.style.border = '1px solid black';
+NewName.style.borderRadius = '5px';
+
 })
